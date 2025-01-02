bacf4c8014981c3bdedb9aa21c4d91c6a6366f80
hardik <165682674+NewLearnerdancer@users.noreply.github.com>
2025-01-02 14:17:00 +0530

    Update Gemini.js
     removing api key

--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -10,8 +10,7 @@ import {
 } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.0-pro";
-const API_KEY = "AIzaSyBNDlFiIV8HQ0wVNTS7ENfRDkrDakSDdOg";
-
+ 
 async function runChat(prompt) {
   const genAI = new GoogleGenerativeAI(API_KEY);
   const model = genAI.getGenerativeModel({ model: MODEL_NAME });
